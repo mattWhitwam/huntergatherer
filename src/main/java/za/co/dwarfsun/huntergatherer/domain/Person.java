d12f0e59572e151fa88b2f16de80e8bb08a8904d
211227307 <211227307@CPT-EN113C03.ADS.CPUT.AC.ZA>
2014-05-06 20:19:53 +0200

    - Created project in class

---
+++ src/main/java/za/co/dwarfsun/huntergatherer/domain/Person.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package za.co.dwarfsun.huntergatherer.domain;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author 211227307
+ */
+@Entity
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique=true)
+    private String displayName;
+    @Embedded //derived from embeddables
+    private int x;
+/*  Embedded objects mean that the entity that object describes
+    can only exist if the in the context of being embedded within another
+    object (i.e. the relationship is such that the embeddable cannot exist in isolation)
+*/
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Person)) {
+            return false;
+        }
+        Person other = (Person) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "za.co.dwarfsun.huntergatherer.domain.Member[ id=" + id + " ]";
+    }
+    
+}
